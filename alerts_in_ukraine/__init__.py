8a6aebcdfec525fd955bf5f69355d784d0d50b65
Пёс <96211333+dmitriypy@users.noreply.github.com>
2022-07-18 17:20:11 +0300

    Update __init__.py

--- alerts_in_ukraine/__init__.py
+++ alerts_in_ukraine/__init__.py
@@ -1,13 +1,20 @@
-__version__ = "1.0"
 import requests
 
 
 class AlertsAPI:
-    def __init__(self, key, id_):
+    def __init__(self, key, id_=None):
         self.key: str = key
-        self.url: str = f"https://alerts.com.ua/api/states/{id_}"
+        self.id: int = id_
+        self.url: str = "https://alerts.com.ua/api/"
 
     def get_alert(self):
-        """Request to API for get info about alert."""
-        r = requests.get(self.url, headers={"X-API-Key": self.key})
+        """Request to API for get info about alerts."""
+        if self.id is None:
+            r = requests.get(self.url + "states", headers={"X-API-Key": self.key})
+        else:
+            r = requests.get(f"{self.url + 'states/' + str(self.id)}", headers={"X-API-Key": self.key})
+        return r.json()
+    
+    def alerts_history(self):
+        r = requests.get(self.url + "history", headers={"X-API-Key": self.key})
         return r.json()
